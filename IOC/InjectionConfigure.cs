4f174616760e3694d7fe47e06c918b7c592c3914
Davi Terceiro <davi.terceiro@hotmail.com>
2021-09-20 15:58:44 -0300

    Adicionar arquivos de projeto.

---
+++ IOC/InjectionConfigure.cs
@@ -0,0 +1,21 @@
+﻿using Data.Repository;
+using Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC
+{
+    public static class Injector
+    {
+        public static void AddDependencyInjectionConfig(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        }
+    }
+}
